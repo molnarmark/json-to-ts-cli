64747df52d771ce146ac8e592c1420ce20818f15
Mark Molnar <molnarpw@gmail.com>
2019-03-10 22:53:23 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# json-to-ts-cli
+
+A tiny module that takes an input file of JSON and outputs the generated TypeScript interfaces to STDOUT.
+
+## Usage
+
+```bash
+npx json-to-ts-cli input.json > interfaces.ts
+```
+
+## Credits
+
+🎉 [Marius Alch](https://github.com/MariusAlch/) for the amazing [json-to-ts](https://github.com/MariusAlch/json-to-ts) module.
---
+++ index.js
@@ -0,0 +1,23 @@
+const JsonToTS = require('json-to-ts');
+const fs = require('fs');
+
+const inputFile = process.argv[2];
+
+if (!inputFile) {
+  return console.error('Input file must be specified.');
+}
+
+let contents;
+try {
+  contents = fs.readFileSync(inputFile).toString();
+} catch (e) {
+  return console.error(e);
+}
+
+try {
+  JsonToTS(JSON.parse(contents)).forEach(typeInterface => {
+    console.log(typeInterface);
+  });
+} catch (e) {
+  return console.error(e);
+}
